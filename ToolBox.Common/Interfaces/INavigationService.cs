875d8e53c49a1b7e31232e850d97f72d6ffcd436
Laurent Julien <digyanoen@gmail.com>
2019-11-18 19:43:43 +0100

    Création projet Toolbox initial

---
+++ ToolBox.Common/Interfaces/INavigationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Toolbox.Common.Interfaces
+{
+    public interface INavigationService<T> where T : Enum
+    {
+        void Test(T test);
+    }
+}
